dece9d2b4a146713b8d52d55761c6ca132e1c202
Ivan Zhang <ivanzhangemail@gmail.com>
2019-04-04 21:16:16 -0700

    Create/Delete Comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -126,4 +126,61 @@ router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (re
                 })
         })
 });
+
+// @route POST api/posts/comment/:id
+// @desc  Add comment to post
+// @access private
+router.post('/comment/:id', passport.authenticate('jwt', { session: false}), (req, res) => {
+
+    const { errors, isValid } = validatePostInput(req.body);
+
+    if (!isValid) {
+        // If any errors, send a 400 with errors object
+        return res.status(400).json(errors);
+    }
+    
+    Post.findById(req.params.id)
+        .then(post => {
+
+            const newComment = {
+                text: req.body.text,
+                name: req.body.name,
+                avatar: req.body.avatar,
+                user: req.user.id
+            }
+
+            // Add user ID to the likes array
+            post.comments.unshift(newComment);
+
+            post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: 'No post found'}));
+
+})
+
+// @route DELETE api/posts/comment/:id/:comment_id
+// @desc  Remove comment to post
+// @access private
+router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', { session: false}), (req, res) => {
+    Post.findById(req.params.id)
+        .then(post => {
+            // Check to see if the comment exists
+            if(post.comments.filter(comment => comment._id.toString() === req.params.comment_id).length > 0){
+                return res.status(404).json({ commentnotexists: "Comment does not exist" });
+            }
+
+            // Get remove index
+            const removeIndex = post.comments
+                .map(item => item._id.toString())
+                .indexOf(req.params.comment_id);
+            
+            // Splice the comment out of the array
+            post.comment.splice(removeIndex, 1);
+
+            post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: 'No post found'}));
+
+})
+
 module.exports = router;
