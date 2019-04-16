40be29ec903dbe35db884f6e03e86c7d331d61e7
Ivan Zhang <izhang@Ivans-MacBook-Pro.local>
2019-04-15 19:33:41 -0700

    Added new landing page

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
